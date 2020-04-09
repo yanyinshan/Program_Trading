342727e7b648087ae204bfa7a8125c3375a32c18
Yinshan Yan <yanyinshanws@gmail.com>
2020-04-08 22:37:51 -0400

    Delete 123.pu.py

--- 123.pu.py
+++
@@ -1,6 +0,0 @@
-print("|" + ticker.center(20) + "|" + str(round(profit, 2)).center(60) + "|")
-profit2.append(profit)
-for y in profit2:
-    total = total + y
-print('-'.center(83, "-"))
-print("|" + 'Total Settled Profit/Loss(RMB):'.center(55) + "|" + str(round(total, 2)).rjust(25) + "|")
